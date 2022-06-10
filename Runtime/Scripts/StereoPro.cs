61dcf1ca32ac446890d8ed42fa416f23c7de4519
harry <hanli.xiong@fun2tech.com>
2022-06-10 19:04:49 +0800

    init

---
+++ Runtime/Scripts/StereoPro.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kmax
+{
+    internal enum FocusMode { Nerver, Once, Always }
+    [RequireComponent(typeof(Camera))]
+    public class StereoPro : MonoBehaviour
+    {
+        Camera _camera;
+        [Header("相机")]
+        [SerializeField] Transform leftEye;
+        [SerializeField] Transform rightEye;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _camera = GetComponent<Camera>();
+            originalProjection = _camera.projectionMatrix;
+            ResetProjection();
+            targetScreen = VisualScreen.Main;
+            targetScreen.ScaleFactor = viewScale;
+            targetScreen.Position = screenPos;
+            if (convergenceMode == FocusMode.Once) Converge();
+        }
+        Matrix4x4 originalProjection;
+        [SerializeField, Header("屏幕")]
+        float screenInch = 24;
+        [SerializeField] Vector3 screenPos;
+        [SerializeField] float viewScale = 1f;
+        private VisualScreen targetScreen;
+        [SerializeField] FocusMode convergenceMode;
+        float left, right, bottom, top, nearClip, farClip;
+        void Update()
+        {
+            if (convergenceMode == FocusMode.Always) Converge();
+        }
+
+        void OnDrawGizmos()
+        {
+            VisualScreen.Main.DrawGizmos();
+        }
+
+        /// <summary>
+        /// 双目聚焦
+        /// </summary>
+        public void Converge()
+        {
+            SetFrustum(transform, _camera.stereoTargetEye);
+            SetFrustum(leftEye, StereoTargetEyeMask.Left);
+            SetFrustum(rightEye, StereoTargetEyeMask.Right);
+        }
+
+        /// <summary>
+        /// 重置投影矩阵
+        /// </summary>
+        public void ResetProjection()
+        {
+            _camera.projectionMatrix = originalProjection;
+        }
+
+        public void SetFrustum(Transform _cam, StereoTargetEyeMask eyeMask)
+        {
+            // _cam.projectionMatrix = TrackScreen(_cam);
+            switch (eyeMask)
+            {
+                case StereoTargetEyeMask.Both:
+                    _camera.projectionMatrix = TrackScreen(_cam);
+                    break;
+                case StereoTargetEyeMask.Left:
+                    _camera.SetStereoViewMatrix(Camera.StereoscopicEye.Left, _cam.worldToLocalMatrix);
+                    _camera.SetStereoProjectionMatrix(Camera.StereoscopicEye.Left, TrackScreen(_cam));
+                    break;
+                case StereoTargetEyeMask.Right:
+                    _camera.SetStereoViewMatrix(Camera.StereoscopicEye.Right, _cam.worldToLocalMatrix);
+                    _camera.SetStereoProjectionMatrix(Camera.StereoscopicEye.Right, TrackScreen(_cam));
+                    break;
+                default: break;
+            }
+        }
+
+        Matrix4x4 TrackScreen(Transform _cam)
+        {
+            if (targetScreen == null) return originalProjection;
+            Vector3 ls = (targetScreen.LeftTop + targetScreen.LeftBottom) / 2f;
+            Vector3 rs = (targetScreen.RightTop + targetScreen.RightBottom) / 2f;
+            Vector3 bs = (targetScreen.LeftBottom + targetScreen.RightBottom) / 2f;
+            Vector3 ts = (targetScreen.LeftTop + targetScreen.RightTop) / 2f;
+            Debug.DrawLine(ls, rs, Color.green);
+            Debug.DrawLine(bs, ts, Color.green);
+            //Debug.DrawLine(ls, _cam.transform.position, Color.gray);
+            //Debug.DrawLine(rs, _cam.transform.position, Color.gray);
+            //Debug.DrawLine(bs, _cam.transform.position, Color.gray);
+            //Debug.DrawLine(ts, _cam.transform.position, Color.gray);
+            // Debug.Log($"world:{ls},{rs},{bs},{ts};");
+            ls -= _cam.transform.position;
+            rs -= _cam.transform.position;
+            bs -= _cam.transform.position;
+            ts -= _cam.transform.position;
+            // Debug.Log($"local:{ls},{rs},{bs},{ts};");
+            // _cam.transform.LookAt(targetScreen);
+            nearClip = _camera.nearClipPlane;
+            farClip = _camera.farClipPlane;
+            float near_m(Vector3 point2cam, Vector3 dir) // 近截面取模
+            {
+                float fm = Vector3.Dot(point2cam, dir);
+                float df = Vector3.Dot(point2cam, _cam.transform.forward);
+                return nearClip * fm / df;
+            }
+            left = near_m(ls, _cam.transform.right);
+            right = near_m(rs, _cam.transform.right);
+            bottom = near_m(bs, _cam.transform.up);
+            top = near_m(ts, _cam.transform.up);
+            return Matrix4x4.Frustum(left, right, bottom, top, nearClip, farClip);
+        }
+        public override string ToString()
+        {
+            return string.Format("original:\n{0}now:\n{1}",
+                originalProjection.ToString("F3"),
+                _camera.projectionMatrix.ToString("F3"));
+        }
+    }
+
+}
---
+++ Runtime/Scripts/VisualScreen.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kmax
+{
+
+    public class VisualScreen
+    {
+        private float screenSize = 24;
+        private float scaleFactor = 1f;
+        const float INCH2M = 0.0254f;//英寸转米
+        float left, right, bottom, top;
+        public Vector3 Position;
+        public float Width => width;
+        public float Height => height;
+        public Vector3 LeftTop => new Vector3(left, top, 0) + Position;
+        public Vector3 RightTop => new Vector3(right, top, 0) + Position;
+        public Vector3 LeftBottom => new Vector3(left, bottom, 0) + Position;
+        public Vector3 RightBottom => new Vector3(right, bottom, 0) + Position;
+        public static VisualScreen Main
+        {
+            get
+            {
+                if (screens == null || screens.Count == 0)
+                {
+                    screens = new List<VisualScreen>();
+                    screens.Add(new VisualScreen(24, 1));
+                }
+                return screens[0];
+            }
+        }
+
+        public float ScreenSize { get => screenSize; set { screenSize = value; CalculateScreenRect();} }
+        public float ScaleFactor { get => scaleFactor; set { scaleFactor = value; CalculateScreenRect(); } }
+        private static List<VisualScreen> screens;
+
+        int widthRatio, heightRatio;
+        float width, height;
+        VisualScreen(float size = 24, float factor = 1f, int wRatio = 16, int hRatio = 9)
+        {
+            screenSize = size;
+            scaleFactor = factor;
+            widthRatio = wRatio;
+            heightRatio = hRatio;
+            CalculateScreenRect();
+        }
+
+        private void CalculateScreenRect()
+        {
+            float size = screenSize * INCH2M;
+            float sizeRatio = Mathf.Sqrt(widthRatio * widthRatio + heightRatio * heightRatio);
+            width = size * widthRatio / sizeRatio * scaleFactor;
+            height = size * heightRatio / sizeRatio * scaleFactor;
+            right = width / 2f;
+            left = -right;
+            top = height / 2f;
+            bottom = -top;
+        }
+
+        public void DrawGizmos()
+        {
+            DrawGizmos(Color.green);
+        }
+        public void DrawGizmos(Color c)
+        {
+            Color oldC = Gizmos.color;
+            Gizmos.color = c;
+            Gizmos.DrawLine(LeftTop, RightTop);
+            Gizmos.DrawLine(LeftBottom, RightBottom);
+            Gizmos.DrawLine(LeftTop, LeftBottom);
+            Gizmos.DrawLine(RightTop, RightBottom);
+            Gizmos.color = oldC;
+        }
+    }
+
+}
